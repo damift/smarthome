f8975f350fad24d3f0dd075dce588967f7aa38c0
abdulbaghdadi <410002024@st.roc.a12.nl>
2026-02-25 10:59:41 +0100

    back to small letter

--- Backend/app/Http/Controllers/AuthController.php
+++ Backend/app/Http/Controllers/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string|min:6|confirmed',
-            'role' => 'sometimes|string|in:Admin,User',
+            'role' => 'sometimes|string|in:admin,user',
         ]);
 
         $user = User::create([
